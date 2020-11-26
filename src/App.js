ab15485c3611f8e5842011f7260827f437e7e794
mehmet temel <temel1072@hotmail.com>
2020-11-26 13:25:58 +0300

    message component done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React, { useState } from 'react'
 import './assets/styles/main.scss'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
+import Login from './components/Login'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
+  const [user, setUser] = useState(null)
   return (
     <div className='app'>
       <Router>
-        <Header />
-        <div className='app__body'>
-          <Sidebar />
-          <Switch>
-            <Route path='/room/:roomId'>
-              <Chat />
-            </Route>
-            <Route path='/'>
-              <Chat />
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className='app__body'>
+              <Sidebar />
+              <Switch>
+                <Route path='/room/:roomId'>
+                  <Chat />
+                </Route>
+                <Route path='/'>
+                  <Chat />
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   )
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../assets/styles/main.scss'
+
+function Login() {
+  return (
+    <div className='login'>
+      <div className='login__container'>
+        <img
+          src='https://cdn.worldvectorlogo.com/logos/slack-new-logo.svg'
+          alt=''
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Login
